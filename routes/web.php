901b85e70f912a58dbc15127659f5b920a5ae975
EdsonAmaya7 <edson_amaya99@live.com>
2022-11-06 07:37:04 -0700

    add traduccion espaniol rutas

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\adminController;
+use App\Http\Controllers\TicketTurnoController;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,12 +15,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [AuthenticatedSessionController::class, 'create'])
+->name('login');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
+
+
+Route::get('/admin', [adminController::class,"index"] )->name('admin.index');
+
+Route::get('viewTicketTurno', [TicketTurnoController::class, 'index'])->name('viewTicketTurno');
+// Route::resource([
+//     'ticketTurno' => TicketTurnoController::class,
+// ]);
+
 
 require __DIR__.'/auth.php';
