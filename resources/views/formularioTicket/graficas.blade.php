194e84b89071bda57a80bb2671d97c014c4740e9
juliiocessar <115444188+juliiocessar@users.noreply.github.com>
2022-11-07 23:44:59 -0600

    graficas

--- resources/views/formularioTicket/graficas.blade.php
+++ resources/views/formularioTicket/graficas.blade.php
@@ -11,20 +11,21 @@ Ticket de Turno
     <div class="card" style="width: 50%; margin: 12% 25%; opacity: .9">
         <input hidden type="text" id="ticket" name="ticket" value="{{ $ticket }}">
         <div style="width: 30rem; height: 30rem" class="p-4">
-            <h3 class="text-center fs-4">Tickets</h3>
-            <canvas id="cantidadTickets" style="width: inherit; height: inherit;"></canvas>
+            <h3 class="text-center fs-4" style="font-weight:bold;">Tickets</h3>
+            <canvas id="cantidadTickets" with="430" height="430"></canvas>
         </div>
+        
     </div>
     <div class="card" style="width: 50%; margin: 12% 25%; opacity: .9">
         <div style="width: 30rem; height: 30rem" class="p-4">
-            <h3 class="text-center fs-4"> Tickets por minicipio </h3>
-            <select name="municipios" id="municipios" onchange="selectMunicipio()">
+            <h3 class="text-center fs-4" style="font-weight:bold;"> Tickets por minicipio </h3>
+            <select name="municipios" id="municipios" onchange="selectMunicipio()" style="background-color: #CCFDF6;">
                 <option value="" selected hidden disabled="">--seleccionar--</option>
                 @foreach ($ticket as $value)
                 <option value="{{ $value->municipio }}">{{ $value->municipio }}</option>
                 @endforeach
             </select>
-            <canvas id="municipioEstado" style="width: inherit; height: inherit;"></canvas>
+            <canvas id="municipioEstado"  with="390" height="390"></canvas>
         </div>
     </div>
 </div>
